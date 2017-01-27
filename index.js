182a9b9881ce9f52ec36ddb887008937b7c14926
Caio Vaccaro <caiovaccaromora@gmail.com>
2017-01-27 16:56:48 -0200

    Building allocation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var getAuth = require('./auth.js'),
     input = require('./services/input'),
     output = require('./services/output-2017-planned'),
-    utils = require('./utils/index');
+    utils = require('./utils/index'),
+    _ = require('lodash'),
+    faker = require('faker/locale/pt_BR');
 
 getAuth()
     .then(function(auth) {
@@ -11,6 +13,9 @@ getAuth()
             })
             .then(function(data) {
                 return calculateAndFillProjects(data, auth);
+            })
+            .then(function(data) {
+                return calculateResourcesAllocation(data);
             });
     }).catch(function(err) {
         console.log(err);
@@ -69,4 +74,95 @@ function calculateAndFillProjects(data, auth) {
         .then(function(data) {
             return output.fillTeamExperienceOutput(auth, data);
         });
+}
+
+
+/**
+ * {
+ *  Role: string
+ *  Type: string
+ *  Level: string
+ *  Available hours: int
+ *  Week 01: int (hours)
+ *  ...
+ * }
+ * 
+ * For each week
+ *  for each team member
+ *      fill his object
+ *      fill the current week
+ */
+function calculateResourcesAllocation(data) {
+    // console.log(JSON.stringify(data.weeks, null, 2));
+    var teamMembers = [];
+
+    try {
+        var weeks = data.weeks,
+            length = weeks.length,
+            i = k = l = 0,
+            n = 1;
+
+        for (i; i < length; i++) {
+            week = weeks[i];
+
+            (function(week) {
+                var prop, resourceQty;
+
+                for (prop in week.team) {
+                    resourceQty = week.team[prop];
+
+                    if (resourceQty > 0) {
+                        (function(resourceQty, prop, week) {
+                            var j = 0;
+
+                            for (j; j < resourceQty; j++) {
+                                (function() {
+                                    var member, weekNum,
+                                        resource = _.find(data.sheetReferenceTeam, { ID: prop }),
+                                        weekNum = week['Week Num'],
+                                        existingMember = _.filter(teamMembers, function(member) {
+                                            return member.role === resource['Role'] && (!member[weekNum] || member[weekNum] < 40)
+                                        });
+
+                                    member = existingMember.length ? existingMember[0] : {
+                                        id: n,
+                                        name: faker.name.findName(),
+                                        role: resource['Role'],
+                                        type: resource['Type'],
+                                        level: resource['Level'],
+                                        availableHours: resource['Available hours']
+                                    };
+
+                                    if (!existingMember.length) n++;
+
+                                    member[weekNum] = member[weekNum] ? parseInt(member[weekNum], 10) + parseInt(member.availableHours, 10) : parseInt(member.availableHours, 10);
+                                    teamMembers.push(member);
+                                })();
+                            }
+                        })(resourceQty, prop, week);
+                    }
+                    // fill weeks without hours with 0
+                }
+            })(week);
+        }
+
+        for (k; k < length; k++) {
+            week = weeks[k];
+
+            (function(week, l) {
+                for (l; l < teamMembers.length; l++) {
+                    if (!teamMembers[l][week['Week Num']]) {
+                        teamMembers[l][week['Week Num']] = 0;
+                    }
+                }
+            })(week, l);
+        }
+
+        console.log(JSON.stringify(teamMembers, null, 2));
+        // data.teamCareerMatrix = teamCareerMatrix;
+
+        return Promise.resolve(data);
+    } catch (err) {
+        return Promise.reject(err);
+    }
 }
